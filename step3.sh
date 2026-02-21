c8c7b20f8094033d0fe8a15bb417c1784f46fd68
ahmed-x86 <ahmdansmr7@gmail.com>
2026-02-21 06:53:17 +0200

    install fonts

--- step3.sh
+++ step3.sh
@@ -1,3 +1,14 @@
-echo -e "🔠 جاري تحديث ذاكرة التخزين المؤقت للخطوط..."
-# إذا كان لديك مجلد خطوط في المستودع سيتم نسخه، ضعه قبل هذا السطر
+#!/bin/bash
+
+echo -e "\n🔥 [Step 3]: Setting up Aesthetics and Fonts..."
+
+echo -e "📦 Installing JetBrains Mono Nerd Font..."
+sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd
+
+echo -e "📦 Installing Cairo Font "
+yay -S --noconfirm ttf-cairo
+
+echo -e "🔠 Updating font cache to ensure perfect text and icon rendering..."
 fc-cache -fv
+
+echo -e "✅ [Step 3]: Fonts and aesthetics setup completed successfully!\n"
