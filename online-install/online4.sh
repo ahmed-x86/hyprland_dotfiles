3e72eeaa75d0ad0804bba24080dadea1a757d4d8
ahmed-x86 <ahmdansmr7@gmail.com>
2026-02-23 20:38:57 +0200

    test

---
+++ online-install/online4.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# الألوان للتنسيق
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e "\n${BLUE}🔥 [Step 4]: Downloading and Deploying Visual Identity Files...${NC}"
