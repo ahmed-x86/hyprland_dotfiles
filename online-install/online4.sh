a711a2034a4c538345b0c68f4997841033b7e5e5
ahmed-x86 <ahmdansmr7@gmail.com>
2026-02-23 20:44:19 +0200

    test

--- online-install/online4.sh
+++ online-install/online4.sh
@@ -9,3 +9,9 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 echo -e "\n${BLUE}🔥 [Step 4]: Downloading and Deploying Visual Identity Files...${NC}"
+
+# مجلد الـ .config في الـ home
+CONFIG_DIR="$HOME/.config"
+
+echo -e "\n${CYAN}📂 Creating necessary directories inside $CONFIG_DIR...${NC}"
+mkdir -p \
