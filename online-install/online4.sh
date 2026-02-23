e6369fcb2ae2310488274f646b3b012a0074d2f6
ahmed-x86 <ahmdansmr7@gmail.com>
2026-02-23 21:01:43 +0200

    test

--- online-install/online4.sh
+++ online-install/online4.sh
@@ -44,4 +44,5 @@ curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.c
 curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/monitors.conf -o "$CONFIG_DIR/hypr/conf/monitors.conf"
 curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/programs.conf -o "$CONFIG_DIR/hypr/conf/programs.conf"
 curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/windowrule.conf -o "$CONFIG_DIR/hypr/conf/windowrule.conf"
-curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/workspaces.conf -o "$CONFIG_DIR/hypr/conf/works
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/workspaces.conf -o "$CONFIG_DIR/hypr/conf/workspaces.conf"
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/hyprland.conf -o "$CONFIG_DIR/hypr/hyprland.conf"
