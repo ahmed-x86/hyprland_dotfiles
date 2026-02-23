e9bef70f61b84a0f262286d3fbc27cbaf3c095bc
ahmed-x86 <ahmdansmr7@gmail.com>
2026-02-23 20:55:50 +0200

    test

--- online-install/online4.sh
+++ online-install/online4.sh
@@ -28,3 +28,14 @@ mkdir -p \
 echo -e "${GREEN}✅ Directories created successfully.${NC}"
 
 echo -e "\n${YELLOW}📥 Fetching dotfiles directly from GitHub... Please wait.${NC}"
+
+# Cava
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/cava/config -o "$CONFIG_DIR/cava/config"
+
+# Hyprland
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/animation.conf -o "$CONFIG_DIR/hypr/conf/animation.conf"
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/appearance.conf -o "$CONFIG_DIR/hypr/conf/appearance.conf"
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/autostart.conf -o "$CONFIG_DIR/hypr/conf/autostart.conf"
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/environment.conf -o "$CONFIG_DIR/hypr/conf/environment.conf"
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/input.conf -o "$CONFIG_DIR/hypr/conf/input.conf"
+curl -fsSL https://raw.githubusercontent.com/ahmed-x86/hyprland_dotfiles/main/.config/hypr/conf/keybinding.conf -o "$CONFIG_DIR/hypr/conf/keybinding.conf"
