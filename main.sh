75e74aabcce3125b1411ac81184a6cdb848abf73
ahmed-x86 <ahmdansmr7@gmail.com>
2026-03-11 13:24:32 +0200

    rmfor test

--- main.sh
+++ main.sh
@@ -90,4 +90,3 @@ echo -e "${PINK}=========================================${WHITE}"
 echo -e "${GREEN}Time elapsed: ${formatted_time}${WHITE}"
 echo -e "${PINK}=========================================${WHITE}"
 echo -e "\n${GREEN}\e[1mmission complete\e[0m\n"
-#for tets
