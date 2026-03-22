b620c3114bdb5414487c4670aac28022cc60d4c9
ahmed-x86 <ahmdansmr7@gmail.com>
2026-03-23 00:08:04 +0200

    add some Features

--- .config/viegphunt/wallpaper_random.sh
+++ .config/viegphunt/wallpaper_random.sh
@@ -1,9 +1,47 @@
 #!/usr/bin/env bash
 
-wallpapers_dir="$HOME/Pictures/Wallpapers"
+WALLPAPERS_DIR="$HOME/Pictures/Wallpapers"
 
-random_wallpaper=$(find "$wallpapers_dir" -maxdepth 1 -type f | shuf -n 1)
+# Check if the wallpapers directory exists
+if [ ! -d "$WALLPAPERS_DIR" ]; then
+    notify-send "Error" "Wallpapers directory not found!"
+    exit 1
+fi
 
-swww img "$random_wallpaper" --transition-type any --transition-duration 2
+# Pick a random wallpaper quickly and safely
+random_wallpaper=$(find "$WALLPAPERS_DIR" -maxdepth 1 -type f | shuf -n 1)
 
-~/.config/viegphunt/wallpaper_effects.sh
+# Ensure a file was actually found
+if [[ -n "$random_wallpaper" && -f "$random_wallpaper" ]]; then
+    
+    # --- The Ultimate Magic Box (All Transitions Mixed + Bezier Curve) ---
+    
+    # Array of ALL cool transitions
+    TRANSITIONS=("grow" "center" "outer" "left" "right" "top" "bottom" "wipe" "wave" "fade")
+    
+    # Pick a truly random transition using 'shuf'
+    RANDOM_TRANSITION=$(printf "%s\n" "${TRANSITIONS[@]}" | shuf -n 1)
+    
+    # Pick a random duration between 1 and 3 seconds
+    RANDOM_DURATION=$(( (RANDOM % 3) + 1 ))
+    
+    # Pick a random angle between 0 and 360 (Only affects wipe and wave)
+    RANDOM_ANGLE=$(( RANDOM % 360 ))
+
+    # Change wallpaper using swww with all the advanced random parameters
+    swww img "$random_wallpaper" \
+        --transition-type "$RANDOM_TRANSITION" \
+        --transition-duration "$RANDOM_DURATION" \
+        --transition-angle "$RANDOM_ANGLE" \
+        --transition-fps 60 \
+        --transition-bezier 0.4,0.2,0.4,1.0
+    
+    # Send a cool notification showing the applied wallpaper and the effect used
+    notify-send -i "$random_wallpaper" "✨ Random Wallpaper Applied" "Effect: $RANDOM_TRANSITION\nDuration: ${RANDOM_DURATION}s"
+    
+    # Run the external effects script
+    ~/.config/viegphunt/wallpaper_effects.sh
+
+else
+    notify-send "Error" "Could not find any wallpapers to apply."
+fi
